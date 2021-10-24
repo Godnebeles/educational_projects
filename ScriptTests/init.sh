fc633ca10908dad9f07afd72865372526dc7d5a1
godnebeles@gmail.com <Godnebeles>
2021-10-24 19:47:46 +0300

    Writed init.sh script

--- ScriptTests/init.sh
+++ ScriptTests/init.sh
@@ -3,18 +3,22 @@ echo "Enter directory name"
 read gitrepo
 if [ -d "$gitrepo" ]
 then
-echo "Repository is exist"
+cd $gitrepo
+echo "You chose this directory: "
+echo $PWD
 else
 mkdir $gitrepo
-echo "Directory created"
+echo "Directory not exist, created new dir \"$gitrepo\""
+echo $PWD
+cd $gitrepo
 fi
 
-cd $gitrepo
 echo "GitHub user.name"
 read name
 echo "GitHub user.email"
 read email
 
+echo "* text=auto" > .gitattributes
 git init .
 git config --global user.email "$name"
 git config --global user.name "$email"
